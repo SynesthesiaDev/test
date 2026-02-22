36a60bebb599e8c6dcc213c45b4bd3b726639abe
SynesthesiaDev <synesthesiadev@proton.me>
2026-02-22 17:58:54 +0100

    aaaaaa

--- Test/Program.cs
+++ Test/Program.cs
@@ -7,9 +7,9 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
         var app = builder.Build();
 
-        var version = "Version 1.2.0";
+        var version = "Version 1.3.0";
 
-        app.MapGet("/", () => $"Unit Status: ONLINE | {version} | Time: {DateTime.Now} | IM GAY!!");
+        app.MapGet("/", () => $"Unit Status: ONLINE | {version} | Time: {DateTime.Now} | IM GAY!! yayyy");
         app.Run();
     }
 }
