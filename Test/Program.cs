6327100398d0c3d79e4a5713c629176ef3864559
SynesthesiaDev <synesthesiadev@proton.me>
2026-02-22 17:06:57 +0100

    a

---
+++ Test/Program.cs
@@ -0,0 +1,15 @@
+namespace Test;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        var version = "Version 1.0.0";
+
+        app.MapGet("/", () => $"Unit Status: ONLINE | {version} | Time: {DateTime.Now}");
+        app.Run();
+    }
+}
